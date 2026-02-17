8114d0ece5a5a857a1e79dd36962146dbd0d1044
Mitch Wheeler <m.wheels@hotmail.com>
2026-02-16 19:39:13 -0600

    chore: update nvim

--- dotfiles/nvim/.config/nvim/lua/config/fzf-lua.lua
+++ dotfiles/nvim/.config/nvim/lua/config/fzf-lua.lua
@@ -7,12 +7,17 @@ require("fzf-lua").setup {
     height = 0.7,
   },
   files = {
+    cmd = "rg --files --hidden --follow --glob '!.git/'",
     previewer = false,
     git_icons = true,
-    no_ignore = false,
+    -- no_ignore = false, -- Not needed due to custom cmd. Otherwise would display hidden files like .gitignore
+  },
+
+  grep = {
+    -- Ensures live_grep also respects .gitignore + shows hidden files
+    rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e --hidden --glob '!.git/'",
   },
 }
-
 
 -- Keybindings
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fuzzy find files" })
