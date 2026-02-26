093871a98179ace7bedc8f13980e2150a583a2ab
Mitch Wheeler <m.wheels@hotmail.com>
2026-02-25 22:27:08 -0600

    refactor: add init catppuccin nvim theme

--- dotfiles/nvim/.config/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/init.lua
@@ -11,17 +11,18 @@ local plugin_specs = {
   {
     "sainnhe/sonokai",
     lazy = false,
-    priority = 1000,     -- load immediately
-    config = function()  -- load early
-        vim.g.sonokai_style = "atlantis"
-        vim.g.sonokai_enable_italic = 1
-        vim.g.sonokai_better_performance = 1
-        -- vim.g.sonokai_transparent_background = 1
+    priority = 1000,    -- load immediately
+    config = function() -- load early
+      vim.g.sonokai_style = "atlantis"
+      vim.g.sonokai_enable_italic = 1
+      vim.g.sonokai_better_performance = 1
+      -- vim.g.sonokai_transparent_background = 1
 
-        -- Load the colorscheme
-        vim.cmd("colorscheme sonokai")
+      -- Load the colorscheme
+      vim.cmd("colorscheme sonokai")
     end,
   },
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- nvim-treesitter
   {
